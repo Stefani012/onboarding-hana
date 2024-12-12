79cb2684a739b980b3b7a6d17d3b426a7b3ec26e
Stefani012 <hana.leonita@cinema21.co.id>
2024-12-12 15:46:14 +0700

    update title sakuku

--- sakuku.sql
+++ sakuku.sql
@@ -1,3 +1,4 @@
+--sakuku ticket
 select 'sakuku','ticket', response_code, case status when 1 then 'Pending' when 2 then 'Success' when 3 then 'Failed' when 4 then 'Billed' when 5 then 'Refund' when 6 then 'User timeout' when 7 then 'Request Refund' else status end as status, count(1),  min(date_entry), max(date_entry) 
 from mtix_application.sakuku_ticket
 where date_entry = date(now())
